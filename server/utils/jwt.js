990b09d3762e3382748a891b41bfdfc1a7fab01c
sushant paneru <panerusushant@gmail.com>
2025-12-28 22:40:06 +0545

    remove unwanted code

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -2,7 +2,6 @@ import jwt from 'jsonwebtoken';
 
 export const jwtUtils = {
     initJwt : async (data) => {
-        console.log(data);
         return jwt.sign(data, process.env.JWT_SECRET_KEY, { expiresIn: "7d" });
     }
 } 
